6371a5a04526b5fe1e593d0a90ebccda8a4b1345
Wojciech Włoch <wojtek.wloch12@gmail.com>
2015-12-14 18:47:31 +0100

    zmiana ukladu katalogow

--- src/Wwloch/Component/Password/Method/Sha1Encoding.php
+++ src/Wwloch/Component/Password/Method/Sha1Encoding.php
@@ -7,13 +7,10 @@ use Wwloch\Component\Password\EncodingMethod;
 
 class Sha1Encoding implements EncodingMethod
 {
-
-public function encode($plainText){
-$sha1 = sha1($plainText);
-
-return $sha1;
-
-}
+	public function encode($plainText){
+		$sha1 = sha1($plainText);
+		return $sha1;
+	}
 
 
 }
